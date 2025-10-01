5e27b3eb6d5de9b31c2e02cbccd924dee4676a6d
Kazik Sobotnicki <Kaziksobo@outlook.com>
2025-10-01 20:06:28 +0100

    Converted to using CustomTkinter library, making a basic home screen gui

--- src/gui.py
+++ src/gui.py
@@ -1,65 +1,81 @@
-import tkinter as tk
 from src.ocr import load_templates, recognise_digit, get_stat_roi
 from src.theme import THEME
+import customtkinter as ctk
 
-class App:
-    '''Main application class for the gaffer's clipboard.
-    '''
+class App(ctk.CTk):
     def __init__(self):
-        '''Initialize the application.
-        '''
-        self.root = tk.Tk()
-        self.root.title("The gaffer's clipboard")
-        self.root.geometry("800x600")
+        super().__init__()
+        self.geometry("800x600")
+        self.minsize(600, 400)
+        # Main Frame
+        self.home_frame = ctk.CTkFrame(
+            self,
+            fg_color=THEME["colors"]["background"]
+        )
+        self.home_frame.pack(expand=True, fill="both")
+        self.home_frame.pack_propagate(False)
+        self.home_frame.pack(expand=True, fill="both")
+        self.home_frame.pack_propagate(False)
         
-        main_menu_frame = tk.Frame(self.root, bg=THEME["colors"]["background"])
-        main_menu_frame.pack(fill="both", expand=True)
-        
-        welcome_label = tk.Label(
-            main_menu_frame,
-            text="Welcome back, gaffer!",
+        # Setting up grid
+        self.home_frame.grid_columnconfigure(0, weight=1)
+        self.home_frame.grid_columnconfigure(1, weight=0)
+        self.home_frame.grid_columnconfigure(2, weight=1)
+        self.home_frame.grid_rowconfigure(0, weight=1)
+        self.home_frame.grid_rowconfigure(4, weight=1)
+
+        # Main Heading
+        self.main_heading = ctk.CTkLabel(
+            self.home_frame, 
+            text="Welcome to Gaffer's Clipboard!",
             font=THEME["fonts"]["title"],
-            fg=THEME["colors"]["primary_text"],
-            bg=THEME["colors"]["background"]
+            text_color=THEME["colors"]["primary_text"]
         )
-        welcome_label.pack(pady=(50, 10))
-
-        sub_label = tk.Label(
-            main_menu_frame,
-            text="What would you like to do",
+        self.main_heading.grid(row=1, column=1, pady=(0, 60))
+        # Question Label
+        self.question_label = ctk.CTkLabel(
+            self.home_frame, text="What would you like to do?",
             font=THEME["fonts"]["body"],
-            fg=THEME["colors"]["secondary_text"],
-            bg=THEME["colors"]["background"]
+            text_color=THEME["colors"]["secondary_text"]
         )
-        sub_label.pack(pady=(0, 50))
+        self.question_label.grid(row=2, column=1, pady=10)
         
-        add_match_button = tk.Button(
-            main_menu_frame,
-            text="Add Match",
-            font=THEME["fonts"]["button"],
-            fg=THEME["colors"]["primary_text"],
-            bg=THEME["colors"]["button_bg"],
-        )
-        add_match_button.pack(pady=10)
-        
-        update_attributes_button = tk.Button(
-            main_menu_frame,
-            text="Update Player Attributes",
-            font=THEME["fonts"]["button"],
-            fg=THEME["colors"]["primary_text"],
-            bg=THEME["colors"]["button_bg"],
-        )
-        update_attributes_button.pack(pady=10)
+        # Buttons Frame
+        self.button_frame = ctk.CTkFrame(
+            self.home_frame,
+            fg_color=THEME["colors"]["background"]
+            )
+        self.button_frame.grid(row=3, column=1, pady=20)
 
-    def capture_and_recognise(self):
-        '''Capture the screen and recognise the stats.
-        '''
-        image_path = "C:\\Users\\kazik\\projects\\OCRtest\\source_images\\defending.png"
-        coords = (704, 105, 722, 128)
-        templates = load_templates()
-        roi_image = get_stat_roi(image_path, coords)
-        recognised_digit = recognise_digit(roi_image, templates)
-        self.info_label.config(text=f"Recognised Digit: {recognised_digit}")
-        
-    def run(self):
-        self.root.mainloop()
+        # Player Update Button
+        self.player_update_button = ctk.CTkButton(
+            self.button_frame, 
+            text="Update player attributes",
+            fg_color=THEME["colors"]["button_bg"],
+            bg_color=THEME["colors"]["background"],
+            font=THEME["fonts"]["button"],
+            text_color=THEME["colors"]["primary_text"],
+            hover_color=THEME["colors"]["accent"]
+        )
+        self.player_update_button.pack(side="left", padx=(0, 10), pady=10)
+
+        # Add Match Button
+        self.add_match_button = ctk.CTkButton(
+            self.button_frame, text="Add new match",
+            fg_color=THEME["colors"]["button_bg"],
+            bg_color=THEME["colors"]["background"],
+            font=THEME["fonts"]["button"],
+            text_color=THEME["colors"]["primary_text"],
+            hover_color=THEME["colors"]["accent"]
+        )
+        self.add_match_button.pack(side="right", padx=(10, 0), pady=10)
+
+    # def capture_and_recognise(self):
+    #     '''Capture the screen and recognise the stats.
+    #     '''
+    #     image_path = "C:\\Users\\kazik\\projects\\OCRtest\\source_images\\defending.png"
+    #     coords = (704, 105, 722, 128)
+    #     templates = load_templates()
+    #     roi_image = get_stat_roi(image_path, coords)
+    #     recognised_digit = recognise_digit(roi_image, templates)
+    #     self.info_label.config(text=f"Recognised Digit: {recognised_digit}")
--- src/theme.py
+++ src/theme.py
@@ -2,14 +2,14 @@ THEME = {
     "colors": {
         "background": "#1e1e1e",
         "primary_text": "#f0f0f0",
-        "secondary_text": "#888888",
+        "secondary_text": "#C5C5C5",
         "accent": "#00bfff",
         "button_bg": "#3c3c3c",
         
     },
     "fonts": {
-        "title": ("Segoe UI", 24, "bold"),
-        "body": ("Segoe UI", 14),
-        "button": ("Segoe UI", 16, "bold")
+        "title": ("Segoe UI", 48, "bold"),
+        "body": ("Segoe UI", 24),
+        "button": ("Segoe UI", 28, "bold")
     }
 }
