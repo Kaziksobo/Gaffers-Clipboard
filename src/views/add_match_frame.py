c6a81faf84e48c6b68792d77a20a3d2602367e43
Kazik Sobotnicki <Kaziksobo@outlook.com>
2025-10-07 20:00:00 +0100

    Created label inside the AddMatchFrame, and added controller logic

--- src/views/add_match_frame.py
+++ src/views/add_match_frame.py
@@ -1,10 +1,15 @@
 import customtkinter as ctk
+from src.theme import THEME
 
 class AddMatchFrame(ctk.CTkFrame):
-    def __init__(self, parent, theme):
+    def __init__(self, parent, controller, theme):
         super().__init__(parent, fg_color=theme["colors"]["background"])
-        
-        self.pack(expand=True, fill="both")
-        self.pack_propagate(False)
-        
-        
+
+        self.label = ctk.CTkLabel(
+            self, 
+            text="Navigate to the match stats screen", 
+            font=theme["fonts"]["title"],
+            text_color=theme["colors"]["primary_text"],
+            anchor="center",
+        )
+        self.label.pack(expand=True)
